01e2032f5492a26570c61a0a84c23bf14716fae0
Matthew Falbo <matthew.falbo@gmail.com>
2014-05-19 12:18:17 -0600

    Part 3 complete. Can add and remove items to/from a wishlist using add/remove buttons and a wishlist item Handlebars template.

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 $(document).on('ready', function() {
 
 	var productTemplate = $('#product-template');
+	var wishlistTemplate = $('#wishlist-template');
 	var templateText = productTemplate.html();
+	var wishlistText = wishlistTemplate.html();
 	console.log(templateText);
+	console.log(wishlistText);
 	var template = Handlebars.compile(templateText);
-	console.log(template);
+	var wishTemplate = Handlebars.compile(wishlistText);
   
-	console.log(productsData);
 	for(var i=0; i<productsData.productsList.length; i++){
 		console.log(productsData.productsList[i]);
 		$('#product-container').append(template(productsData.productsList[i]));
-
 	}
+
+	$('.add-btn').click(function(){
+		var title = $(this).closest($('.product')).find('h2').text();
+		$('#wishlist').append(wishTemplate({title: title}));
+	});
+
+	$(document).on('click','.remove-btn',function(){
+		$(this).closest($('.wish-item')).remove();
+	});
 });
